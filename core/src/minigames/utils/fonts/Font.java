96542a7454e907352b07d267dbc4271fa520e2ff
Matías Steinberg <matiuri@hotmail.com>
2015-03-29 13:09:29 -0300

    Cambiar sistema de fuente Arial
    +Añadido método estático arial a la clase Font
    -Eliminada enumeración Arial de la clase Font

--- core/src/minigames/utils/fonts/Font.java
+++ core/src/minigames/utils/fonts/Font.java
@@ -6,27 +6,15 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class Font {
-	public enum Arial {
-		_24(24), _36(36);
-		
-		private final BitmapFont ARIAL;
-		
-		Arial(int size) {
-			FreeTypeFontParameter par = new FreeTypeFontParameter();
-			par.size = size;
-			ARIAL = loadFont("Fonts/Arial.ttf", par);
-		}
-		
-		public BitmapFont getFont() {
-			return ARIAL;
-		}
+	public static BitmapFont arial(int size) {
+		FreeTypeFontParameter par = new FreeTypeFontParameter();
+		par.size = size;
+		return loadFont("Fonts/Arial.ttf", par);
 	}
-	
-	private static BitmapFont font;
 	
 	public static BitmapFont loadFont(String path, FreeTypeFontParameter parameter) {
 		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(path));
-		font = generator.generateFont(parameter);
+		BitmapFont font = generator.generateFont(parameter);
 		generator.dispose();
 		return font;
 	}
